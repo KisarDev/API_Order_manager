747cb4b92321569b5130a6c0caec48a01fca9752
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-16 09:44:20 -0300

    register product and order_product

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 import api
 
 urlpatterns = [
-    path('register_order/', views.register_order),
+    path('register_order/', views.register_order_product, name='register_order'),
+    path('create_product/', views.create_product, name='create_product'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,27 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.http import HttpResponse
+from rest_framework.response import Response
+from .models import Product, ProductOrder, Order
+from .serializers import ProductSerializer, ProductOrderSerializer, OrderSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def register_order_product(request):
+    if request.method == 'POST':
+        order_product_data = request.data
+        print(order_product_data)
+        serializer = ProductOrderSerializer(data=order_product_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+@api_view(['POST'])
+def create_product(request):
+    if request.method == 'POST':
+        product_data = request.data
+        serializer = ProductSerializer(data=product_data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201) 
+        return Response(serializer.errors, status=400)  
 
-@api_view['POST']
-def register_order(request):
-    objects = {}
-    return Response(objects)
+    
