001d7aaaee21621c0fa7279c56948daa031b617e
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-17 10:40:02 -0300

    Swagger comments

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-
+#Swagger
 schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
-
+#Swagger
 urlpatterns += [
    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
