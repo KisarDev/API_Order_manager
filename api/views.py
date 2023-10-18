a1831213eb0463e3edb8b066af55d78958afd9b5
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-18 10:41:59 -0300

    Documentanting endpoints

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product, ProductOrder, Order
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import ProductSerializer, ProductOrderSerializer, OrderSerializer
 
+@swagger_auto_schema(method="POST", request_body=ProductOrderSerializer)
 @api_view(['POST'])
-def register_order_product(request, produto: ProductOrderSerializer):
+def register_order_product(request):
+    """ Esse endpoint registra ordens de produção"""
     if request.method == 'POST':
         order_product_data = request.data
         print(order_product_data)
@@ -13,8 +15,12 @@ def register_order_product(request, produto: ProductOrderSerializer):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+#------------------------------product------------------------------>
+@swagger_auto_schema(method="POST", request_body=ProductSerializer)
 @api_view(['POST'])
 def create_product(request):
+    """ Esse endpoint registra novos produtos """
     if request.method == 'POST':
         product_data = request.data
         serializer = ProductSerializer(data=product_data)
